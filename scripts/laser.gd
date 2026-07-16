8f8d01b532506601e3b865f35294e894c5d2d7ce
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-07-16 11:17:45 +1000

    added platform with spinning laser

---
+++ scripts/laser.gd
@@ -0,0 +1,11 @@
+extends Area3D
+var spinny_speed: float = 2.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	rotate_y(spinny_speed * delta)
