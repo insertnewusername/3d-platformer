f797f67e625046a766caa09e387ae75a061e85dd
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-07-16 18:01:42 +1000

    correct checkpoin_pos for 5

--- scripts/main.gd
+++ scripts/main.gd
@@ -42,6 +42,6 @@ func _on_checkpoint_4_body_entered(body: Node3D) -> void:
 
 func _on_checkpoint_5_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
-		Main.checkpoint_pos = Vector3(7.395652, 10.01446, -7.101365)
-	
+		Main.checkpoint_pos = Vector3(-22.58336, 10.01441, -7.025383)
+
 		print("checkpoint changed to", Main.checkpoint_pos)
