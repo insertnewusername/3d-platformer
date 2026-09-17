d9ea5d8c90a25d2eb5a97b90c2397586f2cfd3a1
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-09-17 11:33:26 +1000

    Have a trashcan as a teleporter

--- scripts/main.gd
+++ scripts/main.gd
@@ -71,3 +71,8 @@ func _on_checkpoint_8_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
 		Main.checkpoint_pos = Vector3(0.0, 0.492775, 0.0)
 		print("checkpoint changed to", Main.checkpoint_pos)
+
+
+func _on_trashcan_body_entered(body: Node3D) -> void:
+	if body.name == "Player":
+		body.global_position = Vector3(0.0, 0.492775, 0.0)
