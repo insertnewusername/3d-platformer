d795f4759346b342fd0498257330837db4991cdf
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-09-18 19:28:19 +1000

    add bg music level1

--- scripts/world.gd
+++ scripts/world.gd
@@ -3,12 +3,14 @@ extends Node3D
 @onready var fadetransition: ColorRect = $"../CanvasLayer/fadetransition"
 @onready var fade_timer: Timer = $"../CanvasLayer/fadetransition/fade_timer"
 @onready var animation: AnimationPlayer = $"../CanvasLayer/fadetransition/AnimationPlayer"
+@onready var bgmusic: AudioStreamPlayer2D = $"../bgmusic"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	fadetransition.show()
 	fade_timer.start()
 	animation.play("fadein")
+	bgmusic.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,3 +20,7 @@ func _process(delta: float) -> void:
 
 func _on_fade_timer_timeout() -> void:
 	fadetransition.hide()
+
+
+func _on_bgmusic_finished() -> void:
+	bgmusic.play()
