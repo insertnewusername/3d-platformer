e5a42527857d300fddc5950a772f7ccc5bf40ed3
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-09-18 17:59:35 +1000

    do scene for levelselect

--- scripts/levelselect.gd
+++ scripts/levelselect.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+var button_type = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,14 +12,21 @@ func _process(delta: float) -> void:
 
 
 func _on_level_1_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level2.tscn")
-
+	button_type = "level1"
+	$Control/fadetransition.show()
+	$Control/fadetransition/fade_timer.start()
+	$Control/fadetransition/AnimationPlayer.play("fadeout")
 
 func _on_level_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	button_type = "level2"
+	$Control/fadetransition.show()
+	$Control/fadetransition/fade_timer.start()
+	$Control/fadetransition/AnimationPlayer.play("fadeout")
+
 
 
 func _on_button_pressed() -> void:
+	button_type = "menu"
 	$Control/fadetransition.show()
 	$Control/fadetransition/fade_timer.start()
 	$Control/fadetransition/AnimationPlayer.play("fadeout")
@@ -27,4 +34,11 @@ func _on_button_pressed() -> void:
 
 
 func _on_fade_timer_timeout() -> void:
-	get_tree().change_scene_to_file("res://scenes/levelselect.tscn")
+	if button_type == "menu" :
+		get_tree().change_scene_to_file("res://scenes/levelselect.tscn")
+	
+	elif button_type == "level1":
+		get_tree().change_scene_to_file("res://scenes/level2.tscn")
+	
+	elif button_type == "level2":
+		get_tree().change_scene_to_file("res://scenes/main.tscn")
