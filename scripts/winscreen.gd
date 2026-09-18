9a78b65c257832832ed586192e3bd049134eb8c4
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-09-18 19:25:11 +1000

    make fade timer oneshot, also yippe sound effect at the end

--- scripts/winscreen.gd
+++ scripts/winscreen.gd
@@ -3,7 +3,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Control/fadetransition.show()
+	$Control/fadetransition/fade_timer.start()
+	$Control/fadetransition/AnimationPlayer.play("fadein")
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,3 +18,8 @@ func _on_button_pressed() -> void:
 	Main.checkpoint_pos = Vector3(0.0, 0.492775, 0.0)
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 		
+
+
+func _on_fade_timer_timeout() -> void:
+	$Control/fadetransition.hide()
+	$yippee.play()
